c4b21ed03fcb596ada1d88ae57640088a6c1dc7c
Khant Zarni <khantzarni@Leos-Laptop.local>
2024-03-03 12:23:24 +1100

    removed hardcoded items

--- app/screens/ItemDisplayScreen.tsx
+++ app/screens/ItemDisplayScreen.tsx
@@ -12,49 +12,7 @@ import axios from "axios";
 import { DATABASE_URL } from "../utils/Helpers";
 
 export const ItemScreen = () => {
-  // const [products, setProducts] = useState<Item[]>([]);
-
-  // const [filteredProducts, setFilteredProducts] = useState<Item[]>([]);
   const [currProduct, setCurrProduct] = useState<Item>();
-
-  // TODO: DELETE AFTER
-  // const burger: Item = {
-  //   itemID: 1,
-  //   receiptID: 1,
-  //   name: "Hamburger",
-  //   quantity: 4,
-  //   weight: null,
-  //   expiryDate: new Date(),
-  //   price: 5.2,
-  //   healthRating: 3,
-  //   healthComment: "Burgers are not good for you. Eat in moderation.",
-  // };
-
-  // const noodle: Item = {
-  //   itemID: 2,
-  //   receiptID: 2,
-  //   name: "Noodle",
-  //   quantity: 2,
-  //   weight: null,
-  //   expiryDate: new Date(),
-  //   price: 3,
-  //   healthRating: 4,
-  //   healthComment: "Noodles are good for you.",
-  // };
-
-  // const chicken: Item = {
-  //   itemID: 3,
-  //   receiptID: 3,
-  //   name: "Chicken",
-  //   quantity: 5,
-  //   weight: null,
-  //   expiryDate: new Date(2024, 2, 23),
-  //   price: 3,
-  //   healthRating: 4,
-  //   healthComment: "Noodles are good for you.",
-  // };
-
-  // const listOfItems: Item[] = [noodle, burger, chicken];
   const fetchProduct = async (itemID: Number) => {
     try {
       const response = await axios.get(`http://${DATABASE_URL}/items/${itemID}`);
@@ -77,30 +35,6 @@ export const ItemScreen = () => {
       console.log(`Error fetching item: ${error}`);
     }
   };
-
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     try {
-  //       const productsCollection = collection(FIRESTORE_DB, "products");
-  //       const productsSnapshot = await getDocs(productsCollection);
-  //       const fetchedProducts: Product[] = productsSnapshot.docs.map((doc) => ({
-  //         id: doc.id,
-  //         name: doc.data().name,
-  //         description: doc.data().description,
-  //         price: doc.data().price,
-  //         image: doc.data().image,
-  //         discountPrice: doc.data().discountPrice,
-  //       }));
-
-  //       setProducts(fetchedProducts);
-  //       setFilteredProducts(fetchedProducts);
-  //     } catch (error) {
-  //       console.error("Error fetching products:", error);
-  //     }
-  //   };
-
-  //   fetchProducts();
-  // }, []);
 
   return (
     <View style={styles.background}>
